03f14de8283130ef7c61528d81a153a32510ca34
BENJAMIN DARTEY <95862472+Benjilo12@users.noreply.github.com>
2025-02-06 14:40:50 +0000

    How to create a server with node.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+///////////////////////////////////////////////////////////
+/////FILES
 //fs is  file system module
 //how to read write control files in node.js
 //Synchronous
@@ -71,7 +77,7 @@
 //New modern
 //Async
 // Reading the content of the start file with node
-const fs = require("fs");
+
 // fs.readFile("starter/txt/start.txt", (err, data) => {
 //   if (err) {
 //     console.log(err);
@@ -88,32 +94,64 @@ const fs = require("fs");
 // make directories or create directories
 //creates an asset folder
 //if folder does exists then create a new folder else if exist already remove folder
-if (!fs.existsSync("./assets")) {
-  fs.mkdir("./assets", (err) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log("folder created");
-  });
-} else {
-  fs.rmdir("./assets", (err) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log("folder deleted");
-  });
-}
+// if (!fs.existsSync("./assets")) {
+//   fs.mkdir("./assets", (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log("folder created");
+//   });
+// } else {
+//   fs.rmdir("./assets", (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log("folder deleted");
+//   });
+// }
 
-// deleting files
-//fs.unlink for deleting files
-//if file exits delete it or dont run code
-if (fs.existsSync("starter/txt/deleteme.txt")) {
-  fs.unlink("starter/txt/deleteme.txt", (err) => {
-    {
-      if (err) {
-        console.log(err);
-      }
-      console.log("file deleted");
-    }
-  });
-}
+// // deleting files
+// //fs.unlink for deleting files
+// //if file exits delete it or dont run code
+// if (fs.existsSync("starter/txt/deleteme.txt")) {
+//   fs.unlink("starter/txt/deleteme.txt", (err) => {
+//     {
+//       if (err) {
+//         console.log(err);
+//       }
+//       console.log("file deleted");
+//     }
+//   });
+// }
+
+///////////////////////////////////////////////////////////
+/////SERVER
+//creating a server
+//Hyper-Text Transfer Protocol
+//Localhost is like a domain name on the web
+//localhost is what your computer is running  on
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "./overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    const pathName = req.url;
+    res.end("Hello from the server!");
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+///listerning to the server request
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listerning to request on port 8000");
+});
+
+/////////////////////////// Status Codes////////*
+//in the browser console network tab
+//200 - OK
+//301 - Resource moved
+//404 - Not found
+//500 - internal server error
